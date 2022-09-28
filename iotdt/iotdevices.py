90eca3a65aba6cc6b4726855df71a7a06b92caf8
Alejandro Jarabo-Peñas <ale.jarabo.penas@ericsson.com>
2022-09-28 11:40:51 +0000

    Simulation separated from devices definition.

--- iotdt/iotdevices.py
+++ iotdt/iotdevices.py
@@ -6,17 +6,12 @@
 # Contact : ale.jarabo.penas@ericsson.com
 # version ='1.0'
 # ---------------------------------------------------------------------------
-""" IoT Devices Simulation
+""" IoT Devices Definition
 In this module a IoT Device Class implementing a MQTT Client is defined. 
 This class is then inherited by several device subclasses such as robotic arms or scanners, that publish their simulated data
 updates to the MQTT network. The network messages have a JSON format, that includes an SDF description of the device reporting 
 the data. In this SDF description we can find a description of the type of device, the modules it implements, and the type of data 
-these modules report to the network. 
-
-The simulation of the data samples is done through the sampling from random distributions that are adapted to the type of the data (such as temperature, 
-position, or speed). It is also important to mention that these devices generate data independently, instead of being affected by what is being
-observed or measured in other devices, since the focus of these project is not this interaction between devices, but the identification and integration
-of new devices (as well as changes in the current devices) into the Knowledge Base structure.
+these modules report to the network.
 """
 # ---------------------------------------------------------------------------
 # Imports 
@@ -701,68 +696,3 @@ def fill_module_uids(data,module_uids):
     data['time_control'] = {'uid': module_uids[-1], 'timestamp' : datetime.now(tz=None).strftime("%Y-%m-%dT%H:%M:%S")}
     dev_module_uids.append(module_uids[-1])
     return data, dev_module_uids
-
-######################
-######## MAIN ########
-######################
-
-# PRODUCTION LINE
-# Initialization Task
-TagScanner(uid="8a40d136-8401-41bd-9845-7dc8f28ea582").start()
-ProductionControl(uid="3d193d4c-ba9c-453e-b98b-cec9546b9182").start()
-
-# Underpan Configuration Task
-PickUpRobot(uid="5f3333b9-8292-4371-b5c5-c1ec21d0b652").start()
-PieceDetector('underpans',uid="45d289e7-4da6-4c10-aa6e-2c1d48b223e2").start()
-
-# Body Configuration Task
-PickUpRobot(uid="da0ba61c-a9bf-4e0d-b975-33b7b4c5d2e8").start()
-ClampingRobot(uid="5ee2149f-ef6e-402b-937e-8e04a2133cdd").start()
-DrillingRobot(uid="98247600-c4fe-4728-bda6-ed8fadf81af2").start()
-PieceDetector('parts',uid="d7295016-4a54-4c98-a4c1-4f0c7f7614b5").start()
-PoseDetector(uid="2c91bd9d-bdfc-4a6b-b465-575f43897d59").start()
-
-# Vehicle Scanning
-ConfigurationScanner(uid="0d451573-243e-423b-bfab-0f3117f88bd0").start()
-FaultNotifier('configuration',uid="f1b43cb8-127a-43b5-905d-9f145171079es").start()
-
-# Window Milling
-PickUpRobot(uid="6625b9ac-55e2-49c8-ab47-d1da21b5f0b5").start()
-MillingRobot(uid="5ce94c31-3004-431e-97b3-c8f779fb180d").start()
-PoseDetector(uid="1df9566a-2f06-48f0-975f-28058c6784c0").start()
-
-# Quality Check
-QualityScanner(uid="fd9ccbb2-be41-4507-85ac-a431fe886541").start()
-FaultNotifier('quality',uid="5bb02f4b-0dfe-45d4-8a87-e902e6ea0bf6").start()
-
-# Artificial Repair
-RepairControl(uid="4525aa12-06fb-484f-be38-58afb33e1558").start()
-
-# Product Completion
-PickUpRobot(uid="ae5e4ad3-bd59-4dc8-b242-e72747d187d4").start()
-PoseDetector(uid="f2d73019-1e87-48a7-b93c-af0a4fc17994").start()
-
-
-# Tasks Connectors
-ConveyorBelt(uid="fbeaa5f3-e532-4e02-8429-c77301f46470").start()
-ConveyorBelt(uid="f169a965-bb15-4db3-97cd-49b5b641a9fe").start()
-ConveyorBelt(uid="3140ce5c-0d08-4aff-9bb4-14a9e6a33d12").start()
-ConveyorBelt(uid="a6f65d7a-019a-4723-9b81-fb4a163fa23a").start()
-ConveyorBelt(uid="f342e60b-6a54-4f20-8874-89a550ebc75c").start()
-
-
-# SAFETY / ENVIRONMENTAL
-# Indoors Monitorization
-AirQualitySensor(uid="5362cb80-381d-4d21-87ba-af283640fa98").start()
-NoiseSensor(uid="7fc17e8f-1e1c-43f8-a2d1-9ff4bcfbf9ff").start()
-SmokeSensor(uid="5a84f26b-bf77-42d3-ab8a-83a214112844").start()
-SeismicSensor(uid="4f1f6ac2-f565-42af-a186-db17f7ed94c2").start()
-
-# Outdoors Monitorization
-AirQualitySensor(uid="c11c3f56-0f26-415f-a00d-3bb929f5ca20").start()
-RainSensor(uid="70a15d0b-f6d3-4833-b929-74abdff69fa5").start()
-WindSensor(uid="f41db548-3a85-491e-ada6-bab5c106ced6").start()
-
-# Safety Alarms
-IndoorsAlarm(uid="4d36d0c4-891f-44ec-afe1-278258058944").start()
-OutdoorsAlarm(uid="b60108c2-46a3-4b67-9b8d-38586cb3039d").start()
