a9c0d2653ea18e9a9c8e578e986cdfee3eb68d80
Alejandro Jarabo-Peñas <ale.jarabo.penas@ericsson.com>
2023-01-04 11:18:44 +0000

    Changed structure and rewrote README

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # KGIOTDT README
-Digital-Twin Knowledge Handler for IoT Platforms - Based on TypeDB Knowledge Graph (KG)
+# Digital Twin Knowledge Graphs for IoT Platforms
+
+## Introduction
+
+This project aims to create a test environment for emulating a series of IoT devices that generate data. The data is then published to a MQTT broker, which forwards it to the Knowledge Graph Agent. The Knowledge Graph Agent processes the data and uses the `consistency_handler` algorithm to determine the queries to generate automatically in TypeQL language, depending on the message received and the UUID and device class of the sender. These queries are then sent to the TypeDB Knowledge Graph, which is updated accordingly.
+
+## Components
+
+- Test Environment: Emulates a series of IoT devices that generate data
+- MQTT broker: Deployed on a docker container and listens on port 8883
+- Knowledge Graph Agent: Subscribes to the MQTT broker and processes data from IoT devices using the `consistency_handler` algorithm to determine the queries to generate automatically
+- TypeDB Knowledge Graph: Deployed on a docker container and listens on port 1729
+
+## Docker Containers
+
+This project uses the following Docker containers:
+
+- `eclipse-mosquitto:2.0.15` for the MQTT broker, which listens on port 8883 and has volumes mounted at `/storage/mosquitto/config`, `/storage/mosquitto/data`, and `/storage/mosquitto/log`
+- `vaticle/typedb:2.11.1` for the TypeDB Knowledge Graph, which listens on port 80 and has no volumes mounted
+
+## Usage
+
+To use this project, follow these steps:
+
+1. Start the Docker containers by running the following command: `docker-compose -f testbed.yaml up -d`
+2. Start the Knowledge Graph Agent by running the command: `python3 kgagent.py`
+    - Wait for the initial definition of the schema and data population to end. 
+    - Wait for the knowledge graph agent to connect to the broker. 
+    - The process will finish once the agent is ready to process messages. 
+3. Start the Test Environment on a separate terminal (without stopping the kg agent) by running the command: `python3 testenv.py`
+    - The devices will be instantiated as threads and connect to the broker. 
+    - The generation of messsages will begin, and the knowledge graph agent will begin processing them and generating queries to the TypeDB knowledge graph.
--- aux.py
+++ aux.py
@@ -7,33 +7,41 @@
 # version ='1.0'
 # ---------------------------------------------------------------------------
 """ Auxiliary Imports/Variables/Classes/Functions
-Definition of auxiliary elements for the Knowledge Graph Agent (kgagent) module.
+Definition of auxiliary elements to be used by other modules.
 """
 # ---------------------------------------------------------------------------
 # Imports
 import os
 import time
+import re
+import uuid
 from datetime import datetime, timedelta
-from json import JSONEncoder, loads, dump
+from json import JSONEncoder, loads, dump, dumps
 
 import pandas as pd
 import numpy as np
+from numpy import random
 from stumpy import mass
 from thefuzz import fuzz
 
+from typing import Any, List, Dict, Tuple
 from colorama import Fore, Style
 from builtins import print as prnt
 from joblib import Parallel, delayed
 from benedict import benedict
 
+from threading import Thread
 from paho.mqtt import client as mqtt_client
 from typedb.client import TypeDB, SessionType, TransactionType
-from typing import Any, List, Dict, Tuple
 # ---------------------------------------------------------------------------
 
 ###########################
 ######## VARIABLES ########
 ###########################
+
+# Root topics for publishing
+prodline_root   = 'productionline'
+safetyenv_root  = 'safetyenvironmental'
 
 # Server addresses
 kb_addr     =   '0.0.0.0:80'
@@ -49,6 +57,44 @@ sdf_cols = ['thing','thing_desc','obj','obj_desc','prop','prop_desc','prop_type'
 #########################
 ######## CLASSES ########
 #########################
+
+# Class providing ground truth for ambient variables such as temperature, pressure...
+class GroundTruth(Thread) :
+    """
+    A class that provides ground truth values for ambient variables such as temperature, pressure, etc. It is a subclass of the Thread class and updates the ground truth values every 100 milliseconds.
+
+    Attributes:
+        ground_truth_vars (dict): a dictionary containing the names and initial values of the ambient variables.
+
+    Methods:
+        update_ground_truth_vars(self) -> None: updates the ground truth values.
+        get(self, var: str) -> float: returns the current value of the specified variable.
+        run(self) -> None: the method called when the thread is started. It updates the ground truth values and sleeps for 100 milliseconds repeatedly.
+    """
+    # Initialization
+    def __init__(self,ground_truth_vars):
+        Thread.__init__(self)
+        self.ground_truth_vars = {}
+        # Initialize each variable
+        for name, params in ground_truth_vars.items() :
+            mu, sigma = params
+            self.ground_truth_vars[name] = sample_normal_mod(mu,sigma)
+    
+    # New ground truth series samples
+    def update_ground_truth_vars(self) -> None :
+        for name, last_value in self.ground_truth_vars.items():
+            self.ground_truth_vars[name] = get_new_sample(last_value,sigma=0.001)
+    
+    # Get ground truth var current value
+    def get(self, var: str) -> float:
+        return self.ground_truth_vars[var]
+
+    # Thread execution
+    def run(self) -> None :
+        while True : 
+            # Update ground truth series values and sleep for 100ms
+            self.update_ground_truth_vars()
+            time.sleep(0.1)
 
 # TypeDB Client Class
 class TypeDBClient():
@@ -216,7 +262,7 @@ class SDFManager() :
         build_sdf_df(sdf: Dict[str, Any]) -> pd.DataFrame: Add SDF description to a DataFrame.
     """
     # Initialization
-    def __init__(self, path='../iot/sdf/'):
+    def __init__(self, path='sdf/'):
         self.path = path
         self.sdf_cache = {}
     
@@ -406,6 +452,197 @@ def get_closest_devs(noninteg_dev: pd.DataFrame, integ_devs: pd.DataFrame, close
 ######## FUNCTIONS ########
 ###########################
 
+# Get new random sample of time series based on last one
+def get_new_sample(last_sample: float, sigma: float = 0.01) -> float:
+    """Returns a new sample by multiplying the last sample by a random number
+    drawn from a normal distribution with mean 1 and standard deviation sigma.
+    
+    Parameters
+    ----------
+    last_sample (float): The last sample value.
+    sigma (float): The standard deviation of the normal distribution. Default value is 0.01.
+    
+    Returns
+    -------
+    A new sample value, as a float.
+    """
+    return last_sample*random.normal(1,sigma)
+
+# Sine wave
+def sample_sine(offset: float, amp: float, T: float, phi: float) -> float:
+    """Returns a new sample of a sine wave at the current time.
+    
+    Parameters
+    ----------
+    offset (float): The offset of the sine wave.
+    amp (float): The amplitude of the sine wave.
+    T (float): The period of the sine wave.
+    phi (float): The phase of the sine wave.
+    
+    Returns
+    -------
+    A new sample value, as a float.
+    """
+    t = time.perf_counter()
+    return get_new_sample(offset + amp*np.sin((2*np.pi/T)*t + phi))
+
+# Square wave
+def sample_square(offset: float, amp: float, T: float, phi: float) -> float:
+    """Returns a new sample of a square wave at the current time.
+    
+    Parameters
+    ----------
+    offset (float): The offset of the square wave.
+    amp (float): The amplitude of the square wave.
+    T (float): The period of the square wave.
+    phi (float): The phase of the square wave.
+    
+    Returns
+    -------
+    A new sample value, as a float.
+    """
+    t = time.perf_counter()
+    return get_new_sample(offset + amp*np.sign(np.sin((2*np.pi/T)*t + phi)))
+
+# Sawtooth wave
+def sample_triangular(offset: float, amp: float, T: float, phi: float) -> float:
+    """Returns a new sample of a triangular wave at the current time.
+    
+    Parameters
+    ----------
+    offset (float): The offset of the sine wave.
+    amp (float): The amplitude of the sine wave.
+    T (float): The period of the sine wave.
+    phi (float): The phase of the sine wave.
+    
+    Returns
+    -------
+    A new sample value, as a float.
+    """
+    t = time.perf_counter() + phi/(2*np.pi/T)
+    val = offset + 2*amp*((t%(T/2)/T)-0.5) if t%T  < T/2 else offset - 2*amp*((t%(T/2)/T)-0.5)
+    return get_new_sample(val)
+
+# Sawtooth wave
+def sample_sawtooth(offset: float, amp: float, T: float, phi: float) -> float:
+    """Returns a new sample of a sawtooth wave at the current time.
+    
+    Parameters
+    ----------
+    offset (float): The offset of the sawtooth wave.
+    amp (float): The amplitude of the sawtooth wave.
+    T (float): The period of the sawtooth wave.
+    phi (float): The phase of the sawtooth wave.
+    
+    Returns
+    -------
+    A new sample value, as a float.
+    """
+    t = time.perf_counter() + phi/(2*np.pi/T)
+    return get_new_sample(offset + amp*(2*((t%T)/T)-0.5))
+
+# Flip a coin (returns True with prob = prob)
+def coin(prob: float = 0.5) -> bool:
+    """Flips a virtual coin.
+    
+    Parameters
+    ----------
+    prob (float): The probability of the coin returning True. Default value is 0.5.
+    
+    Returns
+    -------
+    True with probability `prob`, False otherwise.
+    """
+    return random.uniform() < prob
+
+# Generate data from a normal distribution between a min and a maximum value
+def sample_normal_mod(mu: float, sigma: float = 0.05, modifier: float = 0.0) -> float:
+    """Generates a random value from a normal distribution between two thresholds.
+    
+    The thresholds are defined as `mu - sigma` and `mu + sigma`,
+    where `mu` is the mean of the normal distribution and `sigma` is its standard deviation.
+    The `modifier` argument is applied to both `mu` and `sigma` before the thresholds are calculated.
+    
+    Parameters
+    ----------
+    mu (float): The mean of the normal distribution.
+    sigma (float): The standard deviation of the normal distribution. Default value is 0.05.
+    modifier (float): A factor to modify `mu` and `sigma` by. Default value is 0.0.
+    
+    Returns
+    -------
+    A random value from the normal distribution, within the calculated thresholds.
+    """
+    # Apply modification factor to values
+    mu = mu*(1 + modifier)
+    sigma = sigma*(1 + modifier)
+    th = [mu - sigma, mu + sigma] # threshold within 1 STD
+
+    # Generate random value within thresholds
+    val = random.normal(mu,sigma)
+    if val > th[0] : return th[0] 
+    elif val < th[1] : return th[1]
+    else: val
+
+# Generate robot data
+def gen_robot_data(offset: float, A: float, T: float, phi: float, actuator_status: bool) -> Dict[str, Dict[str, float]]:
+    """Generates data for a robot.
+    
+    Parameters
+    ----------
+    offset (float): The offset of the generated data.
+    A (float): A scaling factor for the generated data.
+    T (float): A period for the generated data.
+    phi (float): A phase shift for the generated data.
+    actuator_status (bool): The status of the robot's actuator.
+    
+    Returns
+    -------
+    A dictionary containing data for the robot's joint and actuator.
+    """
+    return {
+        'joint': {
+            'x_position' : sample_sine(offset,A,T,phi),
+            'y_position' : sample_sine(offset-1,A*2,T*1.25,phi),  
+            'z_position' : sample_sine(offset-2,A/2,T*0.75,phi),
+            'roll_orientation' : sample_sawtooth(offset+np.pi/2,A,T,phi), 
+            'pitch_orientation' : sample_sawtooth(offset+1*np.pi/4,A*2,T*1.25,phi), 
+            'yaw_orientation' : sample_sawtooth(offset+2*np.pi/4,A/2,T*0.75,phi)
+        },
+        'actuator': {
+            'x_position' : sample_triangular(offset,A,T,phi),
+            'y_position' : sample_triangular(offset-1,A*2,T*1.5,phi),  
+            'z_position' : sample_triangular(offset-2,A/2,T*0.5,phi),
+            'roll_orientation' : sample_triangular(offset+np.pi/2,A,T,phi), 
+            'pitch_orientation' : sample_triangular(offset+1*np.pi/4,A*2,T*1.5,phi), 
+            'yaw_orientation' : sample_triangular(offset+2*np.pi/4,A/2,T*0.75,phi),
+            'actuator_status' : actuator_status
+        }
+    }
+
+# Generate header data
+def gen_header(dev_class: str, topic: str, uuid: str, category: str = 'DATA') -> Dict[str, str]:
+    """Generates header data for a device.
+    
+    Parameters
+    ----------
+    dev_class (str): The class of the device.
+    topic (str): The topic of the device.
+    uuid (str): The UUID of the device.
+    category (str): The category of the data. Default value is 'DATA'.
+    
+    Returns
+    -------
+    A dictionary containing the header data.
+    """
+    return {
+        'category' : category,
+        'class' : dev_class,
+        'topic' : topic,
+        'uuid' : uuid,
+        'timestamp' : datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f")
+    }
+
 # Get all paths in dict with sdfRef
 def get_ref_paths(dic: dict) -> dict:
     """Get all paths in a dictionary to values with a key of 'sdfRef'.
@@ -457,6 +694,27 @@ def build_devs_df(devices: dict) -> pd.DataFrame:
                 rows.append(row.copy())
 
     return pd.DataFrame(rows)
+
+# Print device data
+def print_device_data(timestamp: datetime, data: Dict[str, Dict[str, Any]]) -> None:
+    """Print device data.
+    
+    Parameters
+    ----------
+    timestamp (str): Datetime object representing the current time.
+    data (dict): A dictionary containing device data. The keys are the names of the devices 
+                and the values are dictionaries containing the device properties.
+            
+    Returns
+    -------
+    None.
+    """
+    print(arrow_str + f'[timer]',kind='')
+    print(arrow_str2 + f'(timestamp)<datetime>={timestamp}',kind='')
+    for mname in data :
+        print(arrow_str + f'[{mname}]',kind='')
+        for mproperty in data[mname] :
+            print(arrow_str2 + f'({mproperty})={data[mname][mproperty]}',kind='')
 
 # Print device tree
 def print_device_tree(dev_dict: Dict) -> None:
